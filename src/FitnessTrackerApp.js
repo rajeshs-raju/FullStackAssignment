be719eb2ba5e30de2c5ca62954b38fc626bd4e0c
rajeshs-raju <rajesh54raju@gmail.com>
2024-05-01 12:54:41 +0530

    Updated password

--- src/FitnessTrackerApp.js
+++ src/FitnessTrackerApp.js
@@ -10,7 +10,7 @@ const port = 3000;
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'rajess@BITS23!',
+  password: '<password>',
   database: 'FitnessTracker'
 });
 
